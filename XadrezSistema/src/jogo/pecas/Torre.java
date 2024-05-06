59a96a5a5fc20a85096fba2c5c54fae42e5c1c29
Luis Bussolin <luiscarlosbusso@gmail.com>
2024-05-06 15:30:10 -0300

    Adicionado excecoes na classe tabuleiro

--- XadrezSistema/src/jogo/pecas/Torre.java
+++ XadrezSistema/src/jogo/pecas/Torre.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package jogo.pecas;
 
 import jogo.Cor;
 import jogo.PecaXadrez;
 import tabuleiro.Tabuleiro;
 
-/**
- *
- * @author Ingadev
- */
 public class Torre extends PecaXadrez{
     
     public Torre(Cor cor, Tabuleiro tabuleiro) {
--- XadrezSistema/src/tabuleiro/Tabuleiro.java
+++ XadrezSistema/src/tabuleiro/Tabuleiro.java
@@ -1,20 +1,39 @@
 package tabuleiro;
 
+import tabuleiro.excecoes.ExcecaoTabuleiro;
+
 public class Tabuleiro {
     private int linhas;
     private int colunas;
     private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas ) {
+        if( linhas < 1 || colunas < 1){
+            throw new ExcecaoTabuleiro("Impossivel um tabuleiro ter linha ou colunas menor que zero!");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
     }
 
     public void colocaPeca(Peca novaPeca, Posicao posicao){
-        
+        if( !posicaoExiste(posicao)){
+            throw new ExcecaoTabuleiro("Posicao nao existe");
+        }  
+        if( existePecaNaPosicao(posicao) ){
+            throw new ExcecaoTabuleiro("Ja existe peca nessa posicao");
+        }
         pecas[posicao.getLinha()][posicao.getColuna()] = novaPeca;
         novaPeca.posicao =  posicao;
+    }
+    
+    public Boolean posicaoExiste(Posicao pos){
+        return (pos.getLinha() >= linhas || pos.getLinha() < 0)
+                && (pos.getColuna() >= colunas || pos.getColuna()< 0);
+    }
+    
+    public Boolean existePecaNaPosicao( Posicao posicao ){
+        return pecaTabuleiro(posicao) != null;
     }
     
     public Peca pecaTabuleiro( Integer linha, Integer coluna){
