91ccb2e5f3249762e1877ccd3d388bbbebf0104a
Luis Bussolin <luiscarlosbusso@gmail.com>
2024-05-07 10:10:10 -0300

    cores as pecas

--- XadrezSistema/src/programa/InterfaceTabuleiro.java
+++ XadrezSistema/src/programa/InterfaceTabuleiro.java
@@ -1,13 +1,36 @@
 package programa;
 
+import jogo.Cor;
 import jogo.PecaXadrez;
 
+
+
 public class InterfaceTabuleiro {
+    
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    
     public static void imprimeTabuleiro( PecaXadrez[][] px ){
-        for( int i = 0; i<px.length; i++){
-            System.out.print(8 - i + " " );
-            for( int j = 0; j<px.length; j++){
-                imprimePeca( px[i][j] );
+        for( int i = 0; i < px.length; i++ ){
+            System.out.print( 8 - i + " " );
+            for( int j = 0; j < px.length; j++ ){
+                imprimePeca( px[ i ][ j ] );
             }
             System.out.println();
         }
@@ -15,10 +38,16 @@ public class InterfaceTabuleiro {
     }
     
     public static void imprimePeca( PecaXadrez peca ){
-        if( peca == null){
+        if (peca == null) {
             System.out.print("-");
-        }else{
-            System.out.print(peca);
+        }
+        else {
+            if (peca.getCor()== Cor.BRANCA) {
+                System.out.print(ANSI_WHITE + peca + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_BLACK + peca + ANSI_RESET);
+            }
         }
         System.out.print(" ");
     }
